647dcecb435dd564d6ebc45371a64d8c345d5154
Bogdan Kopcha <34816253+kopchabogdan@users.noreply.github.com>
2022-09-18 14:55:09 +0300

    Made changes from browser

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Date;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("This task done by Bogdan Kopcha SE-19-1/9");
+        System.out.println("This task done by Bogdan Kopcha SE-19-1/9"); /* Моє ім'я та фамілія */
         Date date = new Date();
         System.out.println("Current time: "+date);
     }
