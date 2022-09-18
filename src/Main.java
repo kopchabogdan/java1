d640c6e9af58ac1ed59af2b92faf0afc8896884f
Bogdan Kopcha <kopchabogdan@gmail.com>
2022-09-18 14:51:26 +0300

    Made code changes

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
+import java.util.Date;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("This task done by Bogdan Kopcha SE-19-1/9");
+        Date date = new Date();
+        System.out.println("Current time: "+date);
     }
 }
