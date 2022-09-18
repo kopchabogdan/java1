7c4be37b087e6514b6baca41130bc6a6587e7c75
Bogdan Kopcha <kopchabogdan@gmail.com>
2022-09-18 14:38:41 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("This task done by Bogdan Kopcha SE-19-1/9");
+    }
+}
